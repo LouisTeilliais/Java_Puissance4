f9e9e18326b43fdbb192184db378b80bec902cee
Louis Teillliais <louis.teilliais@gmail.com>
2021-09-27 15:53:23 +0200

    Demande colonne

--- puissance4/src/main/java/puissance4/App.java
+++ puissance4/src/main/java/puissance4/App.java
@@ -1,13 +1,61 @@
 package puissance4;
 
-/**
- * Hello world!
- *
- */
+import java.io.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    
+        // do {
+        //     PlayerTurn("X");
+
+        //     PlayerTurn("O");
+        // }
+        // while(); 
     }
+
+    static void menu(){
+
+
+
+    }
+
+    static String PlayerTurn(String player){
+
+        try {
+            String choose = getStringFromConsole("Player " + player +  ", What column do you choose ?");
+            if ( choose.charAt(0) >= 'a' && choose.charAt(0) <= 'f'){
+                System.out.println(choose);
+                
+            }else {
+                throw new IOException("Bad colummn");
+            } 
+            return choose;
+            
+        }
+        catch(Exception e){
+            System.err.println("Please, input a valid column");
+            return PlayerTurn(player);
+        }
+        
+    }
+
+    static String getStringFromConsole(String message) throws IOException{
+
+        System.out.println(message);
+        System.out.print("> ");
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String input = br.readLine();
+
+        if (input == ""){
+            throw new IOException("Can't be empty");
+        }
+        return input; 
+    }
+
+    
 }
+
+
